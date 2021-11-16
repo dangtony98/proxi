34c162220028dcf9c4437812ec57c026f9766b51
Tuan Dang <tuandang@Tuans-MacBook-Pro.local>
2021-11-15 22:33:47 -0500

    Update

--- index.js
+++ index.js
@@ -19,5 +19,5 @@ app.get('*', (req, res) => {
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-    console.log(`Server is ready at http://localhost:${PORT}`);
+    console.log('Server is running!');
 });
